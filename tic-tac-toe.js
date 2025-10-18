e04fcf464a37fd2d7452cffef37f2fb2a885bbe4
Sydni Miller <millersydni44@gmail.com>
2025-10-17 22:23:45 -0500

    JS file exercise 2 done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,6 +3,34 @@ document.addEventListener('DOMContentLoaded',() => {
     const board = document.getElementById('board');
     const cells= board.querySelectorAll('div');
 
-    cells.forEach(cell=>cell.classLists.add('square'));
+    cells.forEach(cell=>cell.classList.add('square'));
+
+
     
-});
+
+
+
+    let c_player='X';
+    let gamestate=["","","","","","","","",""];
+
+    cells.forEach((cell,index) => {
+        cell.addEventListener('click',() => {
+
+        if(cell.textContent===''){
+            cell.textContent = c_player;
+            cell.classList.add(c_player);
+            gamestate[index]=c_player;
+
+            if (c_player==='X'){
+                c_player ='O';
+            } else {
+                c_player ='X'
+            }
+        }
+
+        });
+
+    });
+
+
+   });
