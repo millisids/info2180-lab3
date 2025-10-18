ac61eb7070309da60906442c951462712197eec6
Sydni Miller <millersydni44@gmail.com>
2025-10-17 22:59:31 -0500

    JS file exercise 4 done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded',() => {
 
     const board = document.getElementById('board');
     const cells= board.querySelectorAll('div');
+    const status=document.getElementById('status');
 
     cells.forEach(cell=>cell.classList.add('square'));
 
@@ -12,14 +13,44 @@ document.addEventListener('DOMContentLoaded',() => {
 
     let c_player='X';
     let gamestate=["","","","","","","","",""];
+    let gameOv = false;
 
+    const wins = [
+     [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7], 
+     [2,5,8],[0,4,8],[2,4,6]
+    ];
+
+    function winner_C(){
+        for (let i=0; i < wins.length; i++) {
+            const a= wins[i][0], b= wins[i][1], c= wins[i][2];
+            if(gamestate[a]!== ""&& gamestate[a]===gamestate[b]&&
+               gamestate[a]===gamestate[c]
+            ) {
+              
+               status.textContent = "Congratulation!"+gamestate[a]+"is the Winner"
+               status.classList.add('you-won');
+               return true;
+
+            }
+
+        }
+        return false;
+    }
+    
     cells.forEach((cell,index) => {
         cell.addEventListener('click',() => {
+
+        if(gameOv)return;
 
         if(cell.textContent===''){
             cell.textContent = c_player;
             cell.classList.add(c_player);
             gamestate[index]=c_player;
+
+            if(winner_C()){
+                gameOv =true;
+                return;
+            }
 
             if (c_player==='X'){
                 c_player ='O';
